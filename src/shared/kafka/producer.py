8225ebeb513ba09e8ffbab0762f8b020e54be727
judeakinwale <judeakinwale@gmail.com>
2026-06-20 18:45:32 +0100

    impl: kafka provider

--- src/shared/kafka/producer.py
+++ src/shared/kafka/producer.py
@@ -1,13 +1,51 @@
 import json
+from typing import Any
+from shared.logging.logger import logger
 from aiokafka import AIOKafkaProducer
 
 
-def create_producer():
-    return AIOKafkaProducer(
-        bootstrap_servers="kafka:9092",
-        value_serializer=lambda v: json.dumps(v).encode("utf-8"),
-        retries=10,
-        retry_backoff_ms=500,
-        request_timeout_ms=1000,
-        acks="all",
-    )
+class KafkaProducer:
+    def __init__(
+        self,
+        bootstrap_servers: str = "kafka:9092",
+        service_name: str = None,
+        overrides: dict[str, Any] | None = None,
+    ):
+        valid_overrides = overrides or {}
+        self._producer = AIOKafkaProducer(
+            bootstrap_servers=bootstrap_servers,
+            value_serializer=lambda v: json.dumps(v).encode("utf-8"),
+            retry_backoff_ms=500,
+            request_timeout_ms=1000,
+            acks="all",
+            **valid_overrides,
+        )
+        self._started = False
+        self.service_name = service_name
+
+    @property
+    def producer(self) -> AIOKafkaProducer:
+        return self._producer
+
+    async def start(self) -> AIOKafkaProducer:
+        await self._producer.start()
+        self._started = True
+        return self._producer
+
+    async def stop(self):
+        await self._producer.stop()
+        self._started = False
+
+    async def publish(self, topic: str, message: dict, key: str = None):
+        try:
+            if not self._started:
+                raise RuntimeError("Producer has not been started")
+
+            await self._producer.send_and_wait(
+                topic, message, key.encode() if key else None
+            )
+        except Exception:
+            logger.exception(
+                f"[{self.service_name}] Error publishing message to topic {topic}"
+            )
+            raise
